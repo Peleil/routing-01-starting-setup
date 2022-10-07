73508144ce5c0de432236431606e673e79adacf1
Yusang Park <usang.park@gmail.com>
2022-10-06 23:21:13 -0700

    params, nested, redirecting functions added

--- src/main.js
+++ src/main.js
@@ -5,16 +5,33 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     {
+      name: 'teams',
       path: '/teams',
       component: TeamsList,
+      // alias: '/',
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
     },
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers },
+
+    {
+      path: '/:notFound(.*)',
+      // redirect: '/',
+      component: NotFound,
+    },
   ],
   linkActiveClass: 'active',
 });
